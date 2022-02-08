fc08e703383d4c4daece0bee16d9eafcc9f31f8c
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-09 00:26:06 +0900

    module

---
+++ 01_basic/greeting.js
@@ -0,0 +1,13 @@
+function welcome(){
+    return 'Hello world';
+}
+
+/*
+in Node.js, method would change because of different host environment.
+*/
+
+/*
+Library is similar with module
+module is logic in program, recyclable
+library is set of logic, set of code (ex. jQuery)
+*/
