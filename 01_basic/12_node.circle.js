aa3626ed665f0f3a7f98fd483736f45e47a89eb3
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-13 15:01:13 +0900

    Add files via upload

---
+++ 01_basic/12_node.circle.js
@@ -0,0 +1,9 @@
+var PI = Math.PI;
+
+exports.area = function (r) {
+    return PI * r * r;
+};
+
+exports.circunference = function (r) {
+    return 2 * PI * r;
+};
---
+++ 01_basic/12_node.demo.js
@@ -0,0 +1,2 @@
+var circle = require('./12_node.circle.js');
+console.log('The area of a circle of radius 4 is ' + circle.area(4));
