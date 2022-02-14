4d98c0ac5b0507021d6c388bc29b0cba3d1cfdd7
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-14 22:38:12 +0900

    Add files via upload

---
+++ 03_object_oriented/08_data_type.js
@@ -0,0 +1,8 @@
+// primitive type
+var str = 'coding';
+console.log(str.length); // 6
+console.log(str.charAt(0)); // c
+
+var str = 'coding';
+str.prop = 'everybody';
+console.log(str.prop); // undefined
